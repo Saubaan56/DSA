4f29e88cc5fef3c8e61559f6501d102d38b554b1
Mohammad Saubaan Bhat <saubaan@Mohammads-MacBook-Air-2.local>
2026-09-23 22:38:01 +0530

    included first.js

---
+++ first.js
@@ -0,0 +1,12 @@
+function greet(name, lastName){
+  console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('John', 'Smith');
+greet('Mary', 'Hail');
+
+function  square(number) {
+   return number * number;
+}
+let number = square(2);
+console.log(number)
